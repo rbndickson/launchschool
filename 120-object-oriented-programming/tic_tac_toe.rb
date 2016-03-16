ee0c251640619e92c2bbde3e6098c3652271ce37
Robin Dickson <robindickson@gmail.com>
2016-03-16 01:06:00 -0500

    Add Tic Tac Toe game from Launchschool tutorial

---
+++ 120-object-oriented-programming/tic_tac_toe.rb
@@ -0,0 +1,214 @@
+class Board
+  WINNING_LINES = [
+    [1, 2, 3], [4, 5, 6], [7, 8, 9],
+    [1, 4, 7], [2, 5, 8], [3, 6, 9],
+    [1, 5, 9], [3, 5, 7]
+  ].freeze
+
+  def initialize
+    @squares = {}
+    (1..9).each { |key| @squares[key] = Square.new }
+  end
+
+  def []=(num, marker)
+   @squares[num].marker = marker
+  end
+
+  def draw
+    puts '     |     |'
+    puts "  #{@squares[1]}  |  #{@squares[2]}  |  #{@squares[3]}"
+    puts '     |     |'
+    puts '-----+-----+-----'
+    puts '     |     |'
+    puts "  #{@squares[4]}  |  #{@squares[5]}  |  #{@squares[6]}"
+    puts '     |     |'
+    puts '-----+-----+-----'
+    puts '     |     |'
+    puts "  #{@squares[7]}  |  #{@squares[8]}  |  #{@squares[9]}"
+    puts '     |     |'
+  end
+
+  def unmarked_keys
+    @squares.keys.select { |key| @squares[key].unmarked? }
+  end
+
+  def full?
+    unmarked_keys.empty?
+  end
+
+  def someone_won?
+    !!winning_marker
+  end
+
+  def markers
+    @squares.values.collect { |square| square.marker unless square.unmarked? }
+  end
+
+  def all_same_marker?(squares)
+    squares.all?(&:marked?) && squares.collect(&:marker).uniq.count == 1
+  end
+
+  def winning_marker
+    WINNING_LINES.each do |line|
+      line_squares = @squares.values_at(*line)
+      return line_squares.first.marker if all_same_marker?(line_squares)
+    end
+    nil
+  end
+
+  def reset
+    (1..9).each { |key| @squares[key] = Square.new }
+  end
+end
+
+class Square
+  INITIAL_MARKER = ' '.freeze
+
+  attr_accessor :marker
+
+  def initialize(marker=INITIAL_MARKER)
+    @marker = marker
+  end
+
+  def marked?
+    marker != INITIAL_MARKER
+  end
+
+  def unmarked?
+    marker == INITIAL_MARKER
+  end
+
+  def to_s
+    marker
+  end
+end
+
+class Player
+  attr_accessor :marker
+
+  def initialize(marker)
+    @marker = marker
+  end
+end
+
+class TTTGame
+  HUMAN_MARKER = 'X'.freeze
+  COMPUTER_MARKER = 'O'.freeze
+  FIRST_TO_MOVE = HUMAN_MARKER
+
+  attr_reader :board, :human, :computer
+
+  def initialize
+    @board = Board.new
+    @human = Player.new(HUMAN_MARKER)
+    @computer = Player.new(COMPUTER_MARKER)
+    @current_marker = FIRST_TO_MOVE
+  end
+
+  def play
+    clear
+    display_welcome_message
+    loop do
+      display_board
+      loop do
+        current_player_moves
+        clear_screen_and_display_board
+        break if board.someone_won? || board.full?
+      end
+      display_result
+      break unless play_again?
+      reset
+      display_play_again_message
+    end
+    display_goodbye_message
+  end
+
+  private
+
+  def current_player_moves
+    if @current_marker == HUMAN_MARKER
+      human_moves
+      @current_marker = COMPUTER_MARKER
+    else
+      computer_moves
+      @current_marker = HUMAN_MARKER
+    end
+  end
+
+  def human_moves
+    puts "Choose a square from: #{board.unmarked_keys.join(', ')}"
+    square = ''
+    loop do
+      square = gets.chomp.to_i
+      break if (board.unmarked_keys).include?(square)
+      puts 'Invalid inputs, please choose an empty square.'
+    end
+    board[square] = human.marker
+  end
+
+  def computer_moves
+    square = board.unmarked_keys.sample
+    board[square] = computer.marker
+  end
+
+  def display_result
+    clear_screen_and_display_board
+    case board.winning_marker
+    when human.marker
+      puts 'You won!'
+    when computer.marker
+      puts 'Computer won!'
+    else
+      puts "It's a tie!"
+    end
+  end
+
+  def play_again?
+    answer = ''
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp.downcase
+      break if ['y', 'n'].include?(answer)
+      puts "Please enter y or n."
+    end
+
+    answer == 'y'
+  end
+
+  def reset
+    board.reset
+    @current_marker = FIRST_TO_MOVE
+    clear
+  end
+
+  def display_play_again_message
+    puts "Let's play again!"
+    puts ''
+  end
+
+  def display_welcome_message
+    puts 'Welcome to Tic Tac Toe!'
+  end
+
+  def display_goodbye_message
+    puts 'Thanks for playing Tic Tac Toe! Goodbye!'
+  end
+
+  def clear
+    system 'clear'
+  end
+
+  def display_board
+    puts "you're #{human.marker}, Computer is #{computer.marker}"
+    puts ''
+    board.draw
+    puts ''
+  end
+
+  def clear_screen_and_display_board
+    clear
+    display_board
+  end
+end
+
+TTTGame.new.play
