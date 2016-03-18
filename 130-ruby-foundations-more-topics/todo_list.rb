169d3b63cdd2ded50375a27e380bf515f0b08f97
Robin Dickson <robindickson@gmail.com>
2016-03-18 10:05:12 -0600

    Add initial todo list

---
+++ 130-ruby-foundations-more-topics/todo_list.rb
@@ -0,0 +1,84 @@
+class Todo
+  DONE_MARKER = 'X'
+  UNDONE_MARKER = ' '
+
+  attr_accessor :title, :description, :done
+
+  def initialize(title, description='')
+    @title = title
+    @description = description
+    @done = false
+  end
+
+  def done!
+    self.done = true
+  end
+
+  def done?
+    done
+  end
+
+  def undone!
+    self.done = false
+  end
+
+  def to_s
+    "[#{done? ? DONE_MARKER : UNDONE_MARKER}] #{title}"
+  end
+end
+
+class TodoList
+  attr_accessor :title
+
+  def initialize(title)
+    @title = title
+    @todos = []
+  end
+
+  def add(todo)
+    raise TypeError unless todo.instance_of? Todo
+    @todos << todo
+  end
+
+  def size
+    @todos.size
+  end
+
+  def first
+    @todos[0]
+  end
+
+  def last
+    @todos[-1]
+  end
+
+  def item_at(index)
+    @todos[index]
+  end
+
+  def mark_done_at(index)
+    @todos[index].done!
+  end
+
+  def mark_undone_at(index)
+    @todos[index].undone!
+  end
+
+  def shift
+    @todos.delete_at(0)
+  end
+
+  def pop
+    @todos.delete_at(-1)
+  end
+
+  def remove_at(index)
+    @todos.delete_at(index)
+  end
+
+  def to_s
+    text = "---- Today's Todos -----\n"
+    text += @todos.join("\n")
+    text
+  end
+end
