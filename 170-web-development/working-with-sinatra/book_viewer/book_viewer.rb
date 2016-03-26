62087c5719f4085de4d26d97ed884b7b76f50e4f
Robin Dickson <robindickson@gmail.com>
2016-03-25 19:39:32 -0600

    Move setting of @contents to before action

--- 170-web-development/working-with-sinatra/book_viewer/book_viewer.rb
+++ 170-web-development/working-with-sinatra/book_viewer/book_viewer.rb
@@ -2,15 +2,17 @@ require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+before do
+  @contents = File.readlines("data/toc.txt")
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
   erb :home
 end
 
 get "/chapters/:number" do
   chapter_number = params[:number]
-  @contents = File.readlines("data/toc.txt")
   chapter_title = @contents[chapter_number.to_i - 1]
   @title = "Chapter #{@chapter_number}: #{chapter_title}"
   @chapter = File.read("data/chp#{chapter_number}.txt")
