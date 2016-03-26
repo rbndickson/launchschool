e9f721b8b80279eb8a97354a388e9b9acd6e0715
Robin Dickson <robindickson@gmail.com>
2016-03-25 18:21:03 -0600

    Add chapter 1 contents

--- 170-web-development/working-with-sinatra/book_viewer/book_viewer.rb
+++ 170-web-development/working-with-sinatra/book_viewer/book_viewer.rb
@@ -7,3 +7,11 @@ get "/" do
   @contents = File.readlines("data/toc.txt")
   erb :home
 end
+
+get "/chapters/1" do
+  @title = "Chapter 1"
+  @contents = File.readlines("data/toc.txt")
+  @chapter = File.read("data/chp1.txt")
+
+  erb :chapter
+end
