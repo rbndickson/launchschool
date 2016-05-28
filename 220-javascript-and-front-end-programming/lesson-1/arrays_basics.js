a9e338dd4bad48ffb97c3ed73a81a1042af42a11
Robin Dickson <robindickson@gmail.com>
2016-05-28 17:22:19 +0100

    Add missing question texts
    And Q and A.

--- 220-javascript-and-front-end-programming/lesson-1/arrays_basics.js
+++ 220-javascript-and-front-end-programming/lesson-1/arrays_basics.js
@@ -1,4 +1,6 @@
 // 1.
+// Write a function that will return the first element of an array that is
+// passed to it as a parameter.
 
 function firstElementOf(arr) {
   return arr[0];
@@ -7,6 +9,8 @@ function firstElementOf(arr) {
 console.log(firstElementOf(["U", "S", "A"])); // Returns "U"
 
 // 2.
+// Write a function that will return the last element of an array that is
+// passed to it as a parameter.
 
 function lastElementOf(arr) {
   return arr[arr.length - 1];
@@ -15,6 +19,10 @@ function lastElementOf(arr) {
 console.log(lastElementOf(["U", "S", "A"])); // Returns "A"
 
 // 3.
+// Write a function that will accept two arguments, an array and an integer
+// representing the position of the element to be returned by the function.
+// What happens when we pass an index greater than the length of the array?
+// What about a negative number?
 
 function nthElementOf(arr, index) {
   return arr[index];
@@ -24,9 +32,21 @@ var digits = [4, 8, 15, 16, 23, 42];
 
 console.log(nthElementOf(digits, 3)); // Returns 16
 console.log(nthElementOf(digits, 8)); // What would this return?
+// undefined
 console.log(nthElementOf(digits, -1)); // What would this return?
+// undefined
+
+// 4.
+// Can we add data into an array at a negative index? If so, why is this
+// possible?
+
+// Yes because arrays are objects in JavaScript.
 
 // 5.
+// Write a function that accepts an array as the first argument and a number as
+// the second. Return a new array of elements that go from the first element
+// and selects elements up to that count. Passing 3, for example, would return
+// the first 3 elements of an array.
 
 function firstNOf(arr, length) {
   return arr.slice(0, length);
@@ -36,6 +56,8 @@ var digits = [4, 8, 15, 16, 23, 42];
 console.log(firstNOf(digits, 3)); // Returns [4, 8, 15]
 
 // 6.
+// Write a function like the previous one, except return the last n elements as
+// a new array.
 
 function lastNOf(arr, length) {
   return arr.slice(arr.length - length);
@@ -45,6 +67,10 @@ var digits = [4, 8, 15, 16, 23, 42];
 console.log(lastNOf(digits, 3)); // Returns [16, 23, 42]
 
 // 7.
+// Using the function we have supplied you in the previous solution, what
+// happens if you pass a count greater than the length of the array? How can
+// you fix the behavior to return a new instance of the entire array if the
+// count is greater than the array length?
 
 function lastNOf(arr, length) {
   var index = arr.length - length;
@@ -57,6 +83,9 @@ var digits = [4, 8, 15, 16, 23, 42];
 console.log(lastNOf(digits, 8)); // Returns [4, 8, 15, 16, 23, 42]
 
 // 8.
+// Write a function that accepts two arrays as arguments and returns an array
+// with the first element in the first array as well as the last element in the
+// second array.
 
 function endsOf(beginning_arr, ending_arr) {
   return [beginning_arr[0], ending_arr[ending_arr.length - 1]];
